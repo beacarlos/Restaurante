a442d19622a33fbe38dd54f8e614bd2215845036
Beatriz Carlos <beatrizacarlos2000@gmail.com>
2020-01-21 22:10:40 -0300

    Bea: Criaçao de seed para mesas e nivel_de_acesso

--- database/migrations/2019_12_23_153325_create_mesas_table.php
+++ database/migrations/2019_12_23_153325_create_mesas_table.php
@@ -16,7 +16,7 @@ class CreateMesasTable extends Migration
         Schema::create('mesas', function (Blueprint $table) {
             $table->increments('mesa_id');
             $table->string('nome')->nullable(false);
-            $table->boolean('disponibilidade')->default(1);
+            $table->boolean('disponibilidade')->default(0);
             $table->timestamp('data_de_criacao')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('ultima_atualizacao')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
---
+++ database/seeds/Mesas.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Mesa;
+
+class Mesas extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        $mesas = Mesa::query()->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        for ($i=1; $i < 13; $i++) { 
+            DB::table('mesas')->insert(['nome'=> 'Mesa '.$i]);
+        }
+
+    }
+}
