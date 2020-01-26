a85aa3f37ac9cfdfe0d4c0e7ed5e84e8ee2dbf65
Beatriz Carlos <beatrizacarlos2000@gmail.com>
2020-01-26 13:16:04 -0300

    bea

--- resources/views/Layouts/dashboard.blade.php
+++ resources/views/Layouts/dashboard.blade.php
@@ -167,7 +167,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+            <img src="{{ asset('/img_perfil/'.Auth::user()->imagem) }}" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
           {{ Auth::user()->nome }}
